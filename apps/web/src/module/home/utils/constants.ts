9e72f50166df06eea6b4fd15879078227b26d776
sahil-dev28 <sahilkolge28@gmail.com>
2026-07-16 14:50:38 +0530

    feat: add constants for hero avatars, services, testimonials, and footer sections

---
+++ apps/web/src/module/home/utils/constants.ts
@@ -0,0 +1,129 @@
+export const HERO_AVATARS = [
+  {
+    src: "avatar-01.svg",
+    left: "5.9%",
+    top: "66.2%",
+    size: "clamp(72px, 11.3vw, 163px)",
+  },
+  {
+    src: "avatar-02.png",
+    left: "14.8%",
+    top: "56.6%",
+    size: "clamp(70px, 11.1vw, 160px)",
+  },
+  {
+    src: "avatar-03.svg",
+    left: "33.6%",
+    top: "27.5%",
+    size: "clamp(78px, 12.2vw, 175px)",
+  },
+  {
+    src: "avatar-04.png",
+    left: "40.1%",
+    top: "75.4%",
+    size: "clamp(74px, 11.7vw, 169px)",
+  },
+  {
+    src: "avatar-05.png",
+    left: "55.9%",
+    top: "37.0%",
+    size: "clamp(74px, 11.7vw, 169px)",
+  },
+  {
+    src: "avatar-06.svg",
+    left: "65.1%",
+    top: "56.1%",
+    size: "clamp(70px, 11vw, 158px)",
+  },
+  {
+    src: "avatar-07.png",
+    left: "79.2%",
+    top: "24.6%",
+    size: "clamp(75px, 11.8vw, 170px)",
+  },
+  {
+    src: "avatar-08.svg",
+    left: "92.0%",
+    top: "62.4%",
+    size: "clamp(75px, 11.8vw, 170px)",
+  },
+] as const;
+
+export const SERVICES = [
+  {
+    label: ["Office of multiple", "interest content"],
+    title: "Collaborative & partnership",
+    href: "#",
+  },
+  {
+    label: ["The hanger US Air force", "digital experimental"],
+    title: "We talk about our weight",
+    href: "#",
+  },
+  {
+    label: ["Delta faucet content,", "social, digital"],
+    title: "Piloting digital confidence",
+    href: "#",
+    decor: "/vectors/circel.png",
+  },
+] as const;
+
+export const TESTIMONIAL_AVATARS = [
+  {
+    src: "/images/testimonials/testimonial-01.png",
+    className: "left-[100px] top-0 h-[104px] w-[104px]",
+  },
+  {
+    src: "/images/testimonials/testimonial-02.png",
+    className: "left-[29px] top-[167px] h-[62px] w-[62px]",
+  },
+  {
+    src: "/images/testimonials/testimonial-03.png",
+    className: "left-[130px] top-[187px] h-[180px] w-[180px]",
+  },
+  {
+    src: "/images/testimonials/testimonial-04.png",
+    className: "left-[8px] top-[361px] h-[102px] w-[102px]",
+  },
+  {
+    src: "/images/testimonials/testimonial-05.png",
+    className: "right-[90px] top-0 h-[122px] w-[122px]",
+  },
+  {
+    src: "/images/testimonials/testimonial-06.png",
+    className: "right-[261px] top-[62px] h-[75px] w-[75px]",
+  },
+  {
+    src: "/images/testimonials/testimonial-07.png",
+    className: "right-[227px] top-[180px] h-[93px] w-[93px]",
+  },
+  {
+    src: "/images/testimonials/testimonial-08.png",
+    className: "right-[27px] top-[278px] h-[220px] w-[220px]",
+  },
+] as const;
+
+export const TESTIMONIAL_REVIEW_LINES = [
+  "Elementum  delivered the site with inthe timeline",
+  "as they requested. Inthe end, the client found a 50%",
+  "increase in traffic with in days since its launch. They",
+  "also had an impressive ability to use technologies that",
+  "the company hasn't used, which have also proved to",
+  "be easy to use and reliable",
+] as const;
+
+export const FOOTER_COMPANY = ["Home", "Studio", "Service", "Blog"] as const;
+
+export const FOOTER_TERMS = [
+  "Privacy Policy",
+  "Terms & Conditions",
+  "Explore",
+  "Accesibility",
+] as const;
+
+export const FOOTER_SOCIAL = [
+  "Instagram",
+  "LinkedIn",
+  "Youtube",
+  "Twitter",
+] as const;
