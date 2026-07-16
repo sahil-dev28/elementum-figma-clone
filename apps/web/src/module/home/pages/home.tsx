7238b7d16218cf997113e97c15f8727d4d2cd7a2
sahil-dev28 <sahilkolge28@gmail.com>
2026-07-16 13:44:41 +0530

    fix: correct import path for Testimonials component

--- apps/web/src/module/home/pages/home.tsx
+++ apps/web/src/module/home/pages/home.tsx
@@ -2,7 +2,7 @@ import Features from "@/module/home/components/features-section/features";
 import Footer from "@/module/home/components/footer/footer";
 import Hero from "@/module/home/components/hero-section/hero";
 import Services from "@/module/home/components/services/services";
-import Testimonials from "@/module/home/components/Testimonials/testimonials";
+import Testimonials from "@/module/home/components/testimonials/testimonials";
 
 export const Home = () => {
   return (
